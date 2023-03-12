e78191370e3eb44d28344167d7d7bb1b8e614590
Johan <jnsoft@users.noreply.github.com>
2023-03-12 22:38:25 +0000

    cleanup

--- tester/run.go
+++ tester/run.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	
 )
 
 func main() {
@@ -10,4 +11,12 @@ func main() {
 	fmt.Println(t.Month())
 	fmt.Println(t.Day())
 	fmt.Println(t.Year())
+
+	state, err := database.NewStateFromDisk()
+	if err != nil {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+	}
+	defer state.Close()
+
 }
